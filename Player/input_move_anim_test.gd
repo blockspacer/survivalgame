1a71ccc5e05a0f6fefc21300a9bb211915cb4b0d
lyubean <lyubean@gmail.com>
2019-09-10 16:40:36 -0700

    Changed stuff Fixed the authorship hopefully! I am me and I committed my own project :)

--- Player/input_move_anim_test.gd
+++ Player/input_move_anim_test.gd
@@ -7,10 +7,14 @@ var move_vec = Vector3(0,0,0) #local location vector that is applied to the Kine
 var look_vec = Vector3(0,0,1) #local location vector that stores where the player should face between frames
 var rot_vec = Vector3(0,0,0) #Euler rotation vector that stores the rotation to be applied to the player
 var moving = false #if the player is moving this frame. for animation and such
+var running = false #if the player is running this frame. for animation and such
 
 var animControl #animation player reference
-var walk_blend_target = 0
-var walk_blend_speed = .2
+var walk_blend_target = 0 # this gets changed in code to be what position in the idle->walk blend the player is lerping towards
+var walk_blend_speed = .2 # amount to lerp between current blend and the target blend
+
+var walk_timescale_target = 0 # target of idle->walk timescale
+var walk_timescale_speed = .2 # amount to lerp between current timescale and the target timescale
 
 var activeCamera #for offsetting movement and rotation with
 export(NodePath) var activeCameraPath
@@ -29,35 +33,48 @@ func _ready():
 #func _physics_process(delta):
 	
 
+#func _input(event):
+	#pass
+
 func _process(delta):
 	
 	## MOVEMENT
 	
 	#If a movement key is pressed, set the move_vec to indicate directions and report movement
 	
+	if Input.is_action_just_pressed("interact"):
+		print("yeah")
+	
+	running = false
+	
+	if Input.is_action_pressed("move_run"): #run
+		running = true
+	
 	moving = false
 	
-	if (Input.is_key_pressed(KEY_W) or (Input.is_key_pressed(KEY_COMMA) or Input.is_key_pressed(KEY_LESS)) or Input.is_key_pressed(KEY_UP)): #up
-		move_vec.z = 1
+	if Input.is_action_pressed("move_up"): #up
+		move_vec.z = Input.get_action_strength("move_up")
 		moving = true
-	elif (Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_O) or Input.is_key_pressed(KEY_DOWN)): #down
-		move_vec.z = -1
+	elif Input.is_action_pressed("move_down"): #down
+		move_vec.z = -Input.get_action_strength("move_down")
 		moving = true
 	else:
 		move_vec.z = 0
-	if (Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT)): #left
-		move_vec.x = 1
+	if Input.is_action_pressed("move_left"): #left
+		move_vec.x = Input.get_action_strength("move_left")
 		moving = true
-	elif (Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_E) or Input.is_key_pressed(KEY_RIGHT)): #right
-		move_vec.x = -1
+	elif Input.is_action_pressed("move_right"): #right
+		move_vec.x = -Input.get_action_strength("move_right")
 		moving = true
 	else:
 		move_vec.x = 0
 	
 	#Normalize vector then adjust angle with camera rotation
 	
-	if (moving == true):
+	if ((move_vec.z==1 or move_vec.z==-1) and (move_vec.x==1 or move_vec.x==-1)):
 		move_vec = move_vec.normalized()
+	
+	if (moving == true):
 		look_vec = move_vec.rotated(Vector3.UP,activeCamera.rotation.y)
 		move_vec = move_vec.rotated(Vector3.UP,activeCamera.rotation.y-PI)
 	
@@ -79,7 +96,10 @@ func _process(delta):
 	
 	if (moving == true):
 		move_and_collide(Vector3 (0 , step_height , 0))
-		move_and_collide(move_vec * move_speed * delta)
+		if running==true:
+			move_and_collide(move_vec * move_speed*2 * delta)
+		elif running==false:
+			move_and_collide(move_vec * move_speed * delta)
 		move_and_collide(Vector3 (0 , -(step_height*2) , 0))
 	
 	## ANIMATION
@@ -91,9 +111,16 @@ func _process(delta):
 		else:
 			walk_blend_target = 0
 		
+		if running:
+			walk_timescale_target = 2
+		else:
+			walk_timescale_target = 1
+		
 		animControl["parameters/walk_blend/blend_amount"] = lerp(animControl["parameters/walk_blend/blend_amount"],walk_blend_target,walk_blend_speed)
 		
-		if Input.is_key_pressed(KEY_E):
+		animControl["parameters/walk_timescale/scale"] = lerp(animControl["parameters/walk_timescale/scale"],walk_timescale_target,walk_timescale_speed)
+		
+		if Input.is_action_pressed("interact"):
 			animControl["parameters/interact_trigger/active"] = 1
 		
 	
